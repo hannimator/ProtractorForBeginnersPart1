c1d40f92118df9381567c4eeaf4b5d98d278344c
Hannah <hannimator@gmail.com>
2015-11-23 23:22:37 +0000

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,12 +1,8 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['homePageSpec.js']
+    specs: ['homePageSpec.js'],
     
     onPrepare: function () {
-    
         browser.driver.manage().window().setSize(1680, 900);
-        
     }
-    
-    
 };
